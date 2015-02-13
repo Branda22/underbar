f6ad40287b616d6a92d1ea3d6d8400c8c4ea40f0
Christian Brandalise <christian.brandalise@gmail.com>
2015-02-12 22:46:11 -0200

    implemented _.some()

--- src/underbar.js
+++ src/underbar.js
@@ -225,23 +225,35 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
-    if(!collection) { return true; }
-    for(var i = 0; i < collection.length; i++){
-      var result;
-      if(iterator){
-        result = iterator(collection[i]);
-      } else {
-        result = collection[i] === true;
+    
+    if(!collection) { 
+      return true; 
+    } else {
+      for(var i = 0; i < collection.length; i++){
+        var result;
+        if(iterator){
+          result = iterator(collection[i]);
+        } else {
+          result = collection[i] === true;
+        }
+        if(!result) { return false };
       }
-      if(!result) { return false };
+    return true; 
     }
-    return true;
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    for(var i = 0; i < collection.length; i++){
+      if(iterator){
+        if (iterator(collection[i])) { return true; }        
+      } else {
+        if (collection[i] === true)   { return true; }
+      }
+    }
+    return false;
   };
 
 
